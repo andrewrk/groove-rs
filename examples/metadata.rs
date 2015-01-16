ca992a92dbcaed33c554da53496f6cddece049f8
Andrew Kelley <superjoe30@gmail.com>
2015-01-16 13:10:45 -0700

    add set_logging api

--- examples/metadata.rs
+++ examples/metadata.rs
@@ -1,7 +1,11 @@
 extern crate groove;
 
+// read or update metadata in a media file
+
 fn main() {
     groove::init();
+    groove::set_logging(groove::Log::Info);
+
     println!("hello");
     groove::finish();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,20 @@ extern crate libc;
 
 use libc::{c_int};
 
+pub enum Log {
+    Quiet,
+    Error,
+    Warning,
+    Info,
+}
+
+impl Copy for Log {}
+
 #[link(name="groove")]
 extern {
     fn groove_init() -> c_int;
     fn groove_finish();
+    fn groove_set_logging(level: c_int);
 }
 
 pub fn init() -> isize {
@@ -15,4 +25,14 @@ pub fn init() -> isize {
 
 pub fn finish() {
     unsafe { groove_finish() }
+}
+
+pub fn set_logging(level: Log) {
+    let c_level: c_int = match level {
+        Log::Quiet   => -8,
+        Log::Error   => 16,
+        Log::Warning => 24,
+        Log::Info    => 32,
+    };
+    unsafe { groove_set_logging(c_level) }
 }
