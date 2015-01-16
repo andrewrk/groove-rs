e8611962d47e30cdcbea4ab3f3a37d6d3d5a6ed8
Andrew Kelley <superjoe30@gmail.com>
2015-01-16 14:58:40 -0700

    add groove_sample_format_bytes_per_sample API

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ extern {
     fn groove_set_logging(level: c_int);
     fn groove_channel_layout_count(channel_layout: uint64_t) -> c_int;
     fn groove_channel_layout_default(count: c_int) -> uint64_t;
+    fn groove_sample_format_bytes_per_sample(format: i32) -> c_int;
 }
 
 pub enum Log {
@@ -66,6 +67,66 @@ impl ChannelLayout {
             CH_LAYOUT_STEREO  => ChannelLayout::LayoutStereo,
             _                 => panic!("invalid channel layout"),
         }
+    }
+}
+
+const SAMPLE_FMT_NONE: i32 = -1;
+const SAMPLE_FMT_U8:   i32 =  0;
+const SAMPLE_FMT_S16:  i32 =  1;
+const SAMPLE_FMT_S32:  i32 =  2;
+const SAMPLE_FMT_FLT:  i32 =  3;
+const SAMPLE_FMT_DBL:  i32 =  4;
+
+const SAMPLE_FMT_U8P:  i32 =  5;
+const SAMPLE_FMT_S16P: i32 =  6;
+const SAMPLE_FMT_S32P: i32 =  7;
+const SAMPLE_FMT_FLTP: i32 =  8;
+const SAMPLE_FMT_DBLP: i32 =  9;
+
+/// how to organize bits which represent audio samples
+pub struct SampleFormat {
+    sample_type: SampleType,
+    /// planar means non-interleaved
+    planar: bool,
+}
+impl Copy for SampleFormat {}
+
+pub enum SampleType {
+    NoType,
+    /// unsigned 8 bits
+    U8,
+    /// signed 16 bits
+    S16,
+    /// signed 32 bits
+    S32,
+    /// float (32 bits)
+    Flt,
+    /// double (64 bits)
+    Dbl,
+}
+impl Copy for SampleType {}
+
+impl SampleFormat {
+    fn to_groove(&self) -> i32 {
+        match (self.sample_type, self.planar) {
+            (SampleType::NoType, false) => SAMPLE_FMT_NONE,
+            (SampleType::U8,     false) => SAMPLE_FMT_U8,
+            (SampleType::S16,    false) => SAMPLE_FMT_S16,
+            (SampleType::S32,    false) => SAMPLE_FMT_S32,
+            (SampleType::Flt,    false) => SAMPLE_FMT_FLT,
+            (SampleType::Dbl,    false) => SAMPLE_FMT_DBL,
+
+            (SampleType::NoType, true)  => SAMPLE_FMT_NONE,
+            (SampleType::U8,     true)  => SAMPLE_FMT_U8P,
+            (SampleType::S16,    true)  => SAMPLE_FMT_S16P,
+            (SampleType::S32,    true)  => SAMPLE_FMT_S32P,
+            (SampleType::Flt,    true)  => SAMPLE_FMT_FLTP,
+            (SampleType::Dbl,    true)  => SAMPLE_FMT_DBLP,
+        }
+    }
+
+    pub fn bytes_per_sample(&self) -> i32 {
+        unsafe { groove_sample_format_bytes_per_sample(self.to_groove()) }
     }
 }
 
